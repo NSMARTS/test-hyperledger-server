0e99f0436ec9621830ef9c53a87feb6e7d4196a8
ljwoon <ljwoon@nsmarts.co.kr>
2024-06-18 17:00:11 +0900

    [feat] 계약서 목록조회, 조회,  업로드, 검증 구현

---
+++ models/contract_schema.js
@@ -0,0 +1,85 @@
+
+const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs'); 
+
+const contract_Schema = mongoose.Schema(
+  {
+    title: {
+      type: String
+    },
+    writer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      index: true
+    },
+    originalname: {
+      type: String
+    },
+    key: {
+      type: String
+
+    },
+    location: {
+      type: String
+
+    },
+    pdfHash: {
+      type: String
+    },
+    receiverA: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      index: true
+    },
+    signA: { // pdf hash + certificate crypto
+      type: String
+    },
+    signPointerA: [{
+      _id: false,
+      pageNum: { type: Object },
+      drawingEvent: {
+        type: Object
+      },
+      signedTime: {
+        type: String
+      }
+    }],
+    statusA: { // pending, read,sign
+      type: String,
+      enum: ['pending', 'read', 'signed'],
+      default: 'pending'
+    },
+    receiverB: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      index: true
+    },
+    signB: {  // pdf hash + certificate crypto
+      type: String
+
+    },
+    signPointerB: [{
+      _id: false,
+      pageNum: { type: Object },
+      drawingEvent: {
+        type: Object
+      },
+      signedTime: {
+        type: String
+      }
+    }],
+    statusB: { // pending, read,sign
+      type: String,
+      enum: ['pending', 'read', 'signed'],
+      default: 'pending'
+    },
+  },
+  {
+    timestamps: true
+  }
+);
+
+
+const Contract = mongoose.model('Contract', contract_Schema)
+
+module.exports = Contract;
